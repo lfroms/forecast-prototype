c7c01d3b609d165a3a64fe6aa2d8cdd8786a9fd0
Lukas Romsicki <lukas@romsicki.com>
2018-10-19 16:19:24 -0400

    Remove unused dependencies

--- Podfile
+++ Podfile
@@ -5,7 +5,6 @@ target 'Forecast' do
 
   # Pods for Forecast
   pod 'SWXMLHash', '~> 4.0.0'
-  pod 'PromiseKit', '~> 6.0'
   pod 'SwiftDate', '~> 5.0'
   pod 'Siesta', '~> 1.0'
 end
--- Pods/Target Support Files/Pods-Forecast/Pods-Forecast-frameworks.sh
+++ Pods/Target Support Files/Pods-Forecast/Pods-Forecast-frameworks.sh
@@ -143,13 +143,11 @@ strip_invalid_archs() {
 
 
 if [[ "$CONFIGURATION" == "Debug" ]]; then
-  install_framework "${BUILT_PRODUCTS_DIR}/PromiseKit/PromiseKit.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/SWXMLHash/SWXMLHash.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/Siesta/Siesta.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/SwiftDate/SwiftDate.framework"
 fi
 if [[ "$CONFIGURATION" == "Release" ]]; then
-  install_framework "${BUILT_PRODUCTS_DIR}/PromiseKit/PromiseKit.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/SWXMLHash/SWXMLHash.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/Siesta/Siesta.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/SwiftDate/SwiftDate.framework"
